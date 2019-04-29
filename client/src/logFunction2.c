88f25bbbe3a814068746f921e722fa6f1c7f33e5
xuyouzhi <xuyouzjsj@163.com>
2019-04-29 15:47:07 +0800

    fixed bugs of several clients download files

--- client/src/logFunction2.c
+++ client/src/logFunction2.c
@@ -1,49 +1,4 @@
 #include "../include/func.h"
-//void getsalt(char *salt,char* passwd)
-//{
-//	int i,j;
-//	for(i=0,j=0;passwd[i]&&j!=3;i++)
-//	{
-//		if(passwd[i]=='$')
-//		{
-//			++j;
-//		}
-//	}
-//	strncpy(salt,passwd,i-1);
-//}
-//
-//
-//int logFunction(char  *argv)
-//{
-//	struct spwd *sp;
-//	char *passwd;
-//	char salt[512]={0};
-//   // if(argc!=2)
-//   // {
-//   // 	printf("error args\n");
-//   // 	return -1;
-//   // }
-//	passwd=getpass("请输入密码:");
-//	sp=getspnam(argv);
-//	if(NULL==sp)
-//	{
-//		perror("getspnam");
-//		return -1;
-//	}
-//	//printf("%-100s\n",sp->sp_pwdp);
-//	getsalt(salt,sp->sp_pwdp);
-//	//puts(salt);
-//	if(!strcmp(sp->sp_pwdp,crypt(passwd,salt)))	
-//	{
-//		printf("登录成功\n");
-//        return 1;
-//	}else{
-//		printf("登录失败\n");
-//        return -1;
-//	}	
-//}
-
-
 
 int GenerateStr(char *salt)
 {
@@ -149,6 +104,7 @@ login2:
         char password[1000]={0};
         memset(ch[0],0,sizeof(ch[0]));   //ch[0]用于存储输入的用户名
         memset(salt,0,sizeof(salt));
+        system("clear");
         printf("-----------------------注册新账号------------------------\n");        
         printf("请输入一个用户名(提示,此用户名将作为你的登录账号)：");
         scanf("%s",ch[0]);
--- server/src/main1.c
+++ server/src/main1.c
@@ -18,20 +18,39 @@ int main(int argc,char *argv[])
     tcpInit(&socketFd,ip,port);
     factoryInit(&f,threadNum,capacity);
     factoryStart(&f);
-
+    int epfd=epoll_create(1);       //创建一个 epoll 句柄
+    printf("epfd=%d\n",epfd);
+    struct epoll_event event,evs[1];            // 目前只监控 socketFd 一个描述符，所以定义的数组长度为1
+    event.events=EPOLLIN|EPOLLET;           //EPOLLIN 表示对应的文件描述符可读(包括对端 SOCKET 正常 关闭)
+    event.data.fd=socketFd;
+    ret=epoll_ctl(epfd,EPOLL_CTL_ADD,socketFd,&event);
+    printf("epoll_ctl ret=%d\n",ret);
+    // 第一个参数是 epoll_create()的返回值
+    //  第二个参数表示动作，用宏来表示，EPOLL_CTL_ADD， 注册新的 fd 到 epfd 中
+    //  第三个参数是需要监听的 fd
+    //   第四个参数是告诉内核需要监听什么事
+     int fdReadNum,i;
     pQue_t pq=&f.que;
     while (1)
-    {
-        memset(&clientAddr,0,sizeof(clientAddr));
-        socklen_t sockLen=sizeof(struct sockaddr);
-        new_fd=accept(socketFd,(struct sockaddr*)&clientAddr,&sockLen);
-        printf("client ip=%s,port=%d\n",inet_ntoa(clientAddr.sin_addr),ntohs(clientAddr.sin_port));
-        pNode_t pnew=(pNode_t)calloc(1,sizeof(Node_t));
-        pnew->new_fd=new_fd;
-        pthread_mutex_lock(&pq->mutex);
-        queInsert(pq,pnew);
-        pthread_mutex_unlock(&pq->mutex);
-        pthread_cond_signal(&f.cond);
+    {                                           //暂时 epoll 只负责监听 socketFd
+        fdReadNum=epoll_wait(epfd,evs,1,-1);       //等待事件的产生，该函数返回需要处理的事情数目
+       printf("fdReadNum=%d\n",fdReadNum);
+        for (i=0;i<fdReadNum;++i)
+        {
+            if (socketFd==evs[i].data.fd)
+            {
+                 memset(&clientAddr,0,sizeof(clientAddr));
+                 socklen_t sockLen=sizeof(struct sockaddr);
+                 new_fd=accept(socketFd,(struct sockaddr*)&clientAddr,&sockLen);
+                 printf("client ip=%s,port=%d\n",inet_ntoa(clientAddr.sin_addr),ntohs(clientAddr.sin_port));
+                 pNode_t pnew=(pNode_t)calloc(1,sizeof(Node_t));
+                 pnew->new_fd=new_fd;
+                 pthread_mutex_lock(&pq->mutex);
+                 queInsert(pq,pnew);
+                 pthread_mutex_unlock(&pq->mutex);
+                 pthread_cond_signal(&f.cond);
+            }
+        }
     }
     return 0;
 }
